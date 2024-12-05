55997787f8c66d88869253f3854361ecc8b505ef
cesar-ioc <cesar.orozco.cervantes@gmail.com>
2024-12-04 18:15:15 -0600

    Initial commit

---
+++ src/main/java/com/talentreef/interviewquestions/takehome/models/Widget.java
@@ -0,0 +1,24 @@
+package com.talentreef.interviewquestions.takehome.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Table
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Builder(toBuilder=true)
+public class Widget {
+
+  private String name;
+
+}
